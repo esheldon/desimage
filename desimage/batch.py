cc9bb0d121c4114a96a8d1d9719624d725f24174
Erin Sheldon <erin.sheldon@gmail.com>
2017-07-27 15:13:13 -0400

    initial commits

---
+++ desimage/batch.py
@@ -0,0 +1,138 @@
+from __future__ import print_function
+import os
+import subprocess
+
+from . import imagemaker
+from . import files
+
+class ScriptMaker(object):
+    def __init__(self, system, campaign, types, missing=False):
+        self._system=system
+        self._campaign=campaign
+        self._missing=missing
+        self._types=types
+
+    def go(self):
+        """
+        write all scripts and batch files
+        """
+        script_dir=files.get_script_dir(self._campaign)
+        if not os.path.exists(script_dir):
+            print("making dir:",script_dir)
+            os.makedirs(script_dir)
+
+        flist = imagemaker.get_flist(self._campaign)
+
+        tilenames={}
+        for key in flist:
+            tilename=key[0:-2]
+            tilenames[tilename] = tilename
+            
+        for tilename in tilenames:
+
+            if self._missing:
+                dowrite=False
+                for type in self._types:
+                    image_file=files.get_output_file(
+                        self._campaign,
+                        tilename,
+                        ext=type,
+                    )
+                    if not os.path.exists(image_file):
+                        doprint=True
+                        break
+            else:
+                doprint=True
+
+            self._write_script(tilename)
+            self._write_batch(tilename)
+
+    def _write_batch(self, tilename):
+        """
+        write the appropriate batch file
+        """
+        if self._system=='wq':
+            self._write_wq(tilename)
+        else:
+            raise ValueError("Unsupported system: '%s'" % self._system)
+
+    def _write_wq(self, tilename):
+        """
+        write the wq script
+        """
+        wq_file=files.get_wq_file(
+            self._campaign,
+            tilename,
+            missing=self._missing,
+        )
+        script_file=files.get_script_file(
+            self._campaign,
+            tilename,
+        )
+        log_file=files.get_log_file(
+            self._campaign,
+            tilename,
+        )
+        job_name='%s' % tilename
+
+        text="""
+command: |
+    . $HOME/.bashrc
+    source activate nsim
+
+    log_file=%(log_file)s
+    rm -f $log_file
+
+    log_dir=$(dirname $log_file)
+    log_base=$(basename $log_file)
+
+    mkdir -vp $log_dir
+
+    log_tmp="$TMPDIR/$log_base"
+    bash %(script_file)s &> $log_tmp
+
+    mv -vf $log_tmp $log_file
+
+mode: bynode
+job_name: "%(job_name)s"
+        \n"""
+
+        text = text % dict(
+            log_file=log_file,
+            script_file=script_file,
+            job_name=job_name,
+        )
+
+        if self._missing:
+            wqlog=wq_file+'.wqlog'
+            if os.path.exists(wqlog):
+                os.remove(wqlog)
+
+        print("writing:",wq_file)
+        with open(wq_file,'w') as fobj:
+            fobj.write(text)
+
+    def _write_script(self, tilename):
+        """
+        write the basic script
+        """
+
+        script_file=files.get_script_file(
+            self._campaign,
+            tilename,
+        )
+
+        typestring=','.join(self._types)
+
+        text="""
+des-make-image --types=%(types)s %(campaign)s %(tilename)s
+        \n"""
+        text = text % dict(
+            campaign=self._campaign,
+            tilename=tilename,
+            types=typestring,
+        )
+
+        print("writing:",script_file)
+        with open(script_file,'w') as fobj:
+            fobj.write(text)
---
+++ desimage/files.py
@@ -0,0 +1,104 @@
+import os
+
+def get_list_dir():
+    """
+    we keep lists here
+    """
+    return os.path.expandvars('$DESDATA/jpg/lists')
+
+def get_flist_file(campaign):
+    """
+    holds paths to coadds
+    """
+    dir=get_list_dir()
+    fname='coadd-flist-%(campaign)s.fits' % dict(
+        campaign=campaign.upper(),
+    )
+    return os.path.join(dir, fname)
+
+def get_base_dir(campaign):
+    """
+    base directory
+    """
+    d='$DESDATA/jpg/%(campaign)s' % dict(
+        campaign=campaign.upper(),
+    )
+    d=os.path.expandvars(d)
+    return d
+
+
+def get_output_dir(campaign, tilename):
+    """
+    location for the image and temp files
+    """
+    bdir=get_base_dir(campaign)
+
+    return os.path.join(bdir, tilename)
+
+def get_temp_dir(campaign, tilename):
+    """
+    location for the image and temp files
+    """
+    d = get_output_dir(campaign, tilename)
+    return os.path.join(d, 'sources')
+
+def get_output_file(campaign, tilename, rebin=None, ext='.jpg'):
+    """
+    location of a output file
+    """
+    odir=get_output_dir(campaign, tilename)
+
+    parts = [
+        tilename,
+        'gri',
+    ]
+
+    if rebin is not None:
+        parts += ['rebin%02d' % rebin]
+
+    front = '-'.join(parts)
+    fname = '%s.%s' % (front,ext)
+
+    return os.path.join(odir, fname)
+
+def get_log_file(campaign, tilename, rebin=None):
+    """
+    file holding log of processing
+    """
+    return get_output_file(
+        campaign,
+        tilename,
+        rebin=rebin,
+        ext='log',
+    )
+#
+# batch processing
+#
+
+def get_script_dir(campaign):
+    """
+    location for scripts
+    """
+    bdir=get_base_dir(campaign)
+    return os.path.join(bdir, 'scripts')
+
+def get_script_file(campaign, tilename):
+    """
+    location for scripts
+    """
+    dir=get_script_dir(campaign)
+    fname='%s.sh' % tilename
+    return os.path.join(dir, fname)
+
+def get_wq_file(campaign, tilename, missing=False):
+    """
+    location for scripts
+    """
+    dir=get_script_dir(campaign)
+    parts=[tilename]
+    if missing:
+        parts += ['-missing']
+
+    fname='-'.join(parts)
+    fname='%s.yaml' % fname
+    return os.path.join(dir, fname)
